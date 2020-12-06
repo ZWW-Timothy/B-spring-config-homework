2864001f90e3380b3c37f4047a321a339f4cdcc4
ZWW-Timothy <zhaowuwei2020@foxmail.com>
2020-12-06 23:02:41 +0800

    [ZWW]: Finish p2s3.

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerLessTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerLessTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("less")
+@TestPropertySource(properties = "levelNumber = 0")
 public class LevelControllerLessTest {
 
     @Autowired
